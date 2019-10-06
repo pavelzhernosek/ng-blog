69920aa9511f36185ddca9f571562289e4626678
Pavel <37225780+pavelzhernosek@users.noreply.github.com>
2019-10-06 21:08:04 +0300

    add post form

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Post } from "../../shared/interfaces";
 
 @Component({
-  selector: 'app-create-page',
-  templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.scss']
+  selector: "app-create-page",
+  templateUrl: "./create-page.component.html",
+  styleUrls: ["./create-page.component.scss"]
 })
 export class CreatePageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required)
+    });
   }
-
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const { title, text, author } = this.form.value;
+    const post: Post = {
+      title,
+      text,
+      author,
+      date: new Date()
+    };
+    console.log(post);
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -4,6 +4,14 @@ export interface User {
   returnSecureToken?: boolean;
 }
 
+export interface Post {
+  id?: string;
+  title: string;
+  text: string;
+  author: string;
+  date: Date;
+}
+
 export interface FbAuthResponse {
   idToken: string;
   expiresIn: string;
